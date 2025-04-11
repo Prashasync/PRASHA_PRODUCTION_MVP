3cfec3316e704e5e143b474f4c40214782b407b4
Prashasync <prasannakumar@prashasync.io>
2025-04-11 00:05:37 -0500

    test

---
+++ src/components/passwordReset/ForgotPassword.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./forgotPassword.css";
+import SendPasswordRequest from "./partials/SendPasswordRequest";
+
+const ForgotPassword = () => {
+  return (
+    <div className="">
+      <SendPasswordRequest />
+    </div>
+  );
+};
+
+export default ForgotPassword;
